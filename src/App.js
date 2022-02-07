0d8bd21060362e85804f7bfd9fa06be71a47e9a2
Karlo Braovac <karlo.braovac@gmail.com>
2022-02-07 23:02:54 +0100

    Adding styling, manipulating and display data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState } from "react"
+import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
+import Header from "./components/Header"
+import feedbackData from './data/FeedbackData'
+
 function App() {
+const[feedback, setFeedback] = useState(feedbackData)
+
     return (
-        <h1>Hello</h1>
+        <>
+          <Header />
+          <div className="container">
+              <FeedbackList feedback={feedback}/>
+          </div>
+        </>
     )
 }
 
