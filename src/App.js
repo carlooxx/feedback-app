1ab021d5c60f6507abe023c23a91101e24e76095
Karlo Braovac <karlo.braovac@gmail.com>
2022-02-08 11:32:42 +0100

    Review and rating funcionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from "react"
-import { FaWindows } from "react-icons/fa"
-import FeedbackItem from "./components/FeedbackItem"
 import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
 import feedbackData from './data/FeedbackData'
+import Feedbackstats from './components/FeedbackStats'
 
 function App() {
 const[feedback, setFeedback] = useState(feedbackData)
@@ -18,6 +17,7 @@ const handleDelete = (id) => {
         <>
           <Header />
           <div className="container">
+              <Feedbackstats feedback={feedback} />
               <FeedbackList feedback={feedback} handleDelete={handleDelete}/>
           </div>
         </>
