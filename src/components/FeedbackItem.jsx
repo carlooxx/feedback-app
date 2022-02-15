ffe8733e9ac796885ab39552f9140c531d0c4edf
Karlo Braovac <karlo.braovac@gmail.com>
2022-02-15 15:55:45 +0100

    Edit feedback item

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,18 @@
 import React, {useContext} from 'react';
-import {useState} from 'react'
 import Card from './shared/Card'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackItem({item}) {
-  const {handleDelete} = useContext(FeedbackContext)
+  const {handleDelete, editFeedback} = useContext(FeedbackContext)
   return (
     <Card reverse={true}>
         <div className="num-display">{item.rating}</div>
         <button onClick={() => handleDelete(item.id)} className='close'>
           <FaTimes color='purple' />
+        </button>
+        <button className='edit' onClick={()=> editFeedback(item)}>
+          <FaEdit color='purple' />
         </button>
         <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -8,22 +8,42 @@ export const FeedbackProvider = ({children}) => {
 const[feedback, setFeedback] = useState([
     {
         id: 1,
-        text: 'This item is from context',
+        text: 'This item is from context 1',
+        rating: 1
+    },
+    {
+        id: 2,
+        text: 'This item is from context 2',
         rating: 4
-    }
+    },
+    {
+        id: 3,
+        text: 'This item is from context 3',
+        rating: 10
+    },
 ])
-const handleDelete = (id) => {
+const[feedbackEdit, setFeedbackEdit] = useState({item: {}, edit: false})
+//delete feedback
+const handleDelete = (id) => { 
     if(window.confirm('Are you sure you want to delete?')) {
         setFeedback(feedback.filter((item) => item.id !== id))
     }
 }
+//add feedback
 const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback])
 }
+//set item to be updated
+const editFeedback = item => {
+    setFeedbackEdit({
+        item,
+        edit: true
+    })
+}
 
     return (
-        <FeedbackContext.Provider value={{feedback, handleDelete, addFeedback}}>
+        <FeedbackContext.Provider value={{feedback, handleDelete, addFeedback, editFeedback}}>
             {children}
         </FeedbackContext.Provider>
     )
